b4cd7951afcb7ee8ab1803adf72ace9fabbd5029
the-robear <robert.wiegmann@gmail.com>
2015-10-17 12:05:02 -0400

    Further work for entity creation

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -5,10 +5,16 @@ class EntitiesController < ApplicationController
       obj[node_property[:name]] = node_property[:value]
     end
     new_node =  @neo.create_node(node_properties)
+    puts new_node
     @neo.add_label(new_node, params[:node_label])
     respond_to do |format|
       format.json do
-        render json: {success: true}
+        render json: {
+          entity_id: new_node["metadata"]["id"],
+          node_label: params[:node_label],
+          node_properties: params[:node_properties],
+          node_properties: params[:node_properties]
+        }
       end
     end
   end
